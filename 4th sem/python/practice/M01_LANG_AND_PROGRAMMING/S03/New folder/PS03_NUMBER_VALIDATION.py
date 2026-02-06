a629aff651514e77586658e56ff9868609f764c3
Siddarth0454 <siddarthshakhai@gmail.com>
2026-02-06 10:33:16 +0530

    message

---
+++ 4th sem/python/practice/M01_LANG_AND_PROGRAMMING/S03/New folder/PS03_NUMBER_VALIDATION.py
@@ -0,0 +1,19 @@
+# # write a code for the factorial of a number
+# n = int(input("enter n:"))
+# fact = 1
+# for i in range(1,n+1):
+#     fact *= i
+# print(fact)
+# write a code to chheck if a number is armstrong or not?
+#  write a code to check if a number is prime or no 
+# n = int(input())
+# if(n <= 1):
+#     print("it is not a prime")
+# else:
+#     for i in range(2,int(n**0.5)+1):
+#         if n % i == 0:
+#             print("not a prime number")
+#             break
+#     else:
+#         print("prime number")
+# reverse of a interger 
